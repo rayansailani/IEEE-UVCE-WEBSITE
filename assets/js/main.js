93f197b8bb3724c3dd5c4972b226ce2a019dc6fe
unknown <rayanbackup1@gmail.com>
2020-11-03 19:49:53 +0530

    removed delay on the preloader

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,7 +11,7 @@
   // Preloader
   $(window).on('load', function () {
     if ($('#preloader').length) {
-      $('#preloader').delay(100).fadeOut('slow', function () {
+      $('#preloader').delay(0).fadeOut('slow', function () {
         $(this).remove();
       });
     }
