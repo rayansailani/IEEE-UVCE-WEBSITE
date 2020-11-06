32f878684c7981bc23415f05f27582f58daa0a02
unknown <rayanbackup1@gmail.com>
2020-11-06 19:48:33 +0530

    EDITED UPDATES SECTION

--- ieee_app/views.py
+++ ieee_app/views.py
@@ -6,9 +6,11 @@ from events.models import Update
 
 def home(request):
     updates = Update.objects.all().order_by('created')
-    updates = updates.filter(till_when__lte=datetime.date.today())
+    updates = updates.filter(till_when__gte=datetime.date.today())
+    today = datetime.date.today()
     context = {
         'updates': updates,
+        "today": today,
     }
     return render(request, 'homepage.html', context)
 
