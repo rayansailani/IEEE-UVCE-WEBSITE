1aa0e5abf59c551c7ab61505489810f6a3e16e46
unknown <rayanbackup1@gmail.com>
2020-11-06 19:07:05 +0530

    allowed hosts

--- ieee_app/settings.py
+++ ieee_app/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = '(=kqy5g@f^hfj(4rq_&*82+bnl+yt0qgjg%7@)!l9mgo3_n8rx'
 DEBUG = True
 
 ALLOWED_HOSTS = ['ieee---uvce.herokuapp.com',
-                 '127.0.0.1', 'ieeeuvce.in']
+                 '127.0.0.1', 'www.ieeeuvce.in']
 
 
 # Application definition
