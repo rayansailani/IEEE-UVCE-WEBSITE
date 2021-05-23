dc54183ef3c2ad40e73620a8e291997b392975a6
Mohammed Rayan sailani <rayanbackup1@gmail.com>
2021-05-23 20:12:30 +0530

    removed DB

--- ieee_app/settings.py
+++ ieee_app/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
-import dj_database_url
+# import dj_database_url
 from pathlib import Path
 import os
 import sys
@@ -105,8 +105,8 @@ DATABASES = {
 }
 
 
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
+# db_from_env = dj_database_url.config(conn_max_age=600)
+# DATABASES['default'].update(db_from_env)
 
 
 # Password validation
