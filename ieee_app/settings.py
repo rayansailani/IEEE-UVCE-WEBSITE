7ac1d9be740fad2a5d4d5fa5c3923bdbad089195
Mohammed Rayan sailani <rayanbackup1@gmail.com>
2021-07-09 22:06:57 +0530

    images testing with URL

--- ieee_app/settings.py
+++ ieee_app/settings.py
@@ -145,13 +145,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'assets'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 )
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 MEDIA_URL = "/media/"
 
