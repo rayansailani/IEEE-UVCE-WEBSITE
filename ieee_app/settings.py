5328637b1e81b4e62c55eeab2f5d799209b5e40f
Mohammed Rayan sailani <rayanbackup1@gmail.com>
2021-07-09 22:26:40 +0530

    changed static dirs again

--- ieee_app/settings.py
+++ ieee_app/settings.py
@@ -149,10 +149,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'staticfiles'),
+    os.path.join(BASE_DIR, 'assets'),
 )
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = "/media/"
 
