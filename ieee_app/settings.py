fe1814a458a3e7cc3448020ffa8cdcf5ff948b4a
Mohammed Rayan sailani <rayanbackup1@gmail.com>
2021-09-14 20:03:49 +0530

    tests assets

--- ieee_app/settings.py
+++ ieee_app/settings.py
@@ -149,10 +149,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'assets'),
+    # os.path.join(BASE_DIR, 'assets'),
 )
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 MEDIA_URL = "/media/"
 
