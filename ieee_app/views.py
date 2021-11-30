c0ee58aba53bde69d51d27544e5d4605c83d6f5e
Mohammed Rayan sailani <rayanbackup1@gmail.com>
2021-11-30 18:52:00 +0530

    added different twitter handles

--- ieee_app/views.py
+++ ieee_app/views.py
@@ -38,7 +38,31 @@ def global_events_view(request):
     access_token_secret= 'lhSZdeH6zNTCVdq6ybwt4ezEEfp4gtY6Q63KhiAkUoJAt'
     auth = tw.OAuthHandler(consumer_key, consumer_secret)
     api = tw.API(auth, wait_on_rate_limit=True)
-    tweets = api.user_timeline(screen_name = "ieee_section", count=10,include_rts = True, tweet_mode = 'extended')
-    context = {"tweets": tweets}
+    tweet_information = [{
+        'name':'IEEE Bangalore Section',
+        'handle':'ieee_section',
+        'tweets': api.user_timeline(screen_name = "ieee_section", count=10,include_rts = True, tweet_mode = 'extended'),
+        'more':'https://twitter.com/ieee_section'
+    },
+    {
+        'name':'IEEE Computer Society',
+        'handle':'ComputerSociety',
+        'tweets': api.user_timeline(screen_name = "ComputerSociety", count=10,include_rts = True, tweet_mode = 'extended'),
+        'more':'https://twitter.com/ComputerSociety'
+    },
+    {
+        'name':'IEEE PES',
+        'handle':'ieee_pes',
+        'tweets': api.user_timeline(screen_name = "ieee_pes", count=10,include_rts = True, tweet_mode = 'extended'),
+        'more':'https://twitter.com/ieee_pes'
+    },
+    {
+        'name':'IEEE PELS',
+        'handle':'IEEEPELS',
+        'tweets': api.user_timeline(screen_name = "IEEEPELS", count=10,include_rts = True, tweet_mode = 'extended'),
+        'more':'https://twitter.com/IEEEPELS'
+    },
+    ]
+    context = {"tweet_info":tweet_information }
     return render(request, "tweets.html", context)
 
